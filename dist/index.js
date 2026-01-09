3c893424b8eeaa7a1f9767c44b2d3f7b83079f08
suyash rao <suyashrao1310@gmail.com>
2026-01-09 23:25:41 +0530

    3rd commit

--- dist/index.js
+++ dist/index.js
@@ -7,17 +7,28 @@ const ws_1 = require("ws");
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const wss = new ws_1.WebSocketServer({ port: 8080 });
-let userCount = 0;
 let allSockets = [];
 wss.on("connection", (socket) => {
-    allSockets.push(socket);
-    userCount = userCount + 1;
-    console.log("user connected #" + userCount);
     socket.on("message", (message) => {
-        console.log("message recieved" + message.toString());
-        for (let i = 0; i < allSockets.length; i++) {
-            const s = allSockets[i];
-            s.send(message.toString() + ": sent from the server");
+        const parsedMessage = JSON.parse(message);
+        if (parsedMessage.type == "join") {
+            allSockets.push({
+                socket,
+                room: parsedMessage.payload.roomId,
+            });
+        }
+        if (parsedMessage.type == "chat") {
+            let currentUserRoom = null;
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i].socket == socket) {
+                    currentUserRoom = allSockets[i].room;
+                }
+            }
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i].room == currentUserRoom) {
+                    allSockets[i].socket.send(parsedMessage.payload.message);
+                }
+            }
         }
     });
 });
